dfc6a667e4ebdc525917ad23b4f04c690872f34e
rwawine <rubchenya.work@gmail.com>
2025-06-26 01:28:24 +0300

    Enhance ProductDetail component by updating Swiper configuration to conditionally enable pagination and navigation based on image count, improving user experience on mobile and desktop views.

--- src/app/catalog/[productId]/ProductDetail.tsx
+++ src/app/catalog/[productId]/ProductDetail.tsx
@@ -205,11 +205,11 @@ export default function ProductDetail({ product, relatedProducts }: ProductDetai
                 modules={[Navigation, Thumbs, Pagination]}
                 spaceBetween={10}
                 navigation={false} // отключаем стандартные стрелки
-                pagination={isMobile ? { clickable: true } : false}
+                pagination={isMobile && images.length > 1 ? { clickable: true } : false}
                 thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
                 className={styles.mainSwiper}
-                loop={true}
-                grabCursor={true}
+                loop={images.length > 1}
+                grabCursor={images.length > 1}
                 onSlideChange={swiper => setActiveIndex(swiper.realIndex)}
                 onAfterInit={swiper => setActiveIndex(swiper.realIndex)}
               >
@@ -246,18 +246,20 @@ export default function ProductDetail({ product, relatedProducts }: ProductDetai
                 </div>
               )}
               {/* Стрелки всегда видимы */}
-              <div className={styles.swiperNavigation}>
-                <button aria-label="Prev" onClick={handlePrev} className={styles.prevBtn} type="button">
-                  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15.4637 5.4593L8.923 12L15.4637 18.5408C15.5126 18.5896 15.5553 18.6422 15.5919 18.6976C15.8482 19.0858 15.8054 19.6133 15.4637 19.955C15.0732 20.3455 14.4401 20.3455 14.0495 19.955L6.80168 12.7071C6.61415 12.5196 6.50879 12.2653 6.50879 12C6.50879 11.7348 6.61415 11.4805 6.80168 11.2929L14.0495 4.04509C14.4401 3.65457 15.0732 3.65457 15.4637 4.04509C15.8543 4.43561 15.8543 5.06878 15.4637 5.4593Z" fill="white" />
-                  </svg>
-                </button>
-                <button aria-label="Next" onClick={handleNext} className={styles.nextBtn} type="button">
-                  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.54778 18.5408L15.0885 12L8.54778 5.4593C8.49896 5.41049 8.45625 5.35788 8.41964 5.30243C8.18883 4.95287 8.20053 4.49031 8.45472 4.1522C8.48279 4.11487 8.5138 4.07906 8.54778 4.04509C8.65218 3.94069 8.77392 3.8642 8.90373 3.81562C9.0774 3.75062 9.26552 3.73558 9.44588 3.7705C9.63497 3.80711 9.81554 3.89864 9.96199 4.04509L17.2098 11.2929C17.3974 11.4805 17.5027 11.7348 17.5027 12C17.5027 12.2653 17.3974 12.5196 17.2098 12.7072L9.96199 19.955C9.57146 20.3455 8.9383 20.3455 8.54778 19.955C8.49896 19.9062 8.45625 19.8536 8.41964 19.7981C8.16335 19.41 8.20607 18.8825 8.54778 18.5408Z" fill="white" />
-                  </svg>
-                </button>
-              </div>
+              {images.length > 1 && (
+                <div className={styles.swiperNavigation}>
+                  <button aria-label="Prev" onClick={handlePrev} className={styles.prevBtn} type="button">
+                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.4637 5.4593L8.923 12L15.4637 18.5408C15.5126 18.5896 15.5553 18.6422 15.5919 18.6976C15.8482 19.0858 15.8054 19.6133 15.4637 19.955C15.0732 20.3455 14.4401 20.3455 14.0495 19.955L6.80168 12.7071C6.61415 12.5196 6.50879 12.2653 6.50879 12C6.50879 11.7348 6.61415 11.4805 6.80168 11.2929L14.0495 4.04509C14.4401 3.65457 15.0732 3.65457 15.4637 4.04509C15.8543 4.43561 15.8543 5.06878 15.4637 5.4593Z" fill="white" />
+                    </svg>
+                  </button>
+                  <button aria-label="Next" onClick={handleNext} className={styles.nextBtn} type="button">
+                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M8.54778 18.5408L15.0885 12L8.54778 5.4593C8.49896 5.41049 8.45625 5.35788 8.41964 5.30243C8.18883 4.95287 8.20053 4.49031 8.45472 4.1522C8.48279 4.11487 8.5138 4.07906 8.54778 4.04509C8.65218 3.94069 8.77392 3.8642 8.90373 3.81562C9.0774 3.75062 9.26552 3.73558 9.44588 3.7705C9.63497 3.80711 9.81554 3.89864 9.96199 4.04509L17.2098 11.2929C17.3974 11.4805 17.5027 11.7348 17.5027 12C17.5027 12.2653 17.3974 12.5196 17.2098 12.7072L9.96199 19.955C9.57146 20.3455 8.9383 20.3455 8.54778 19.955C8.49896 19.9062 8.45625 19.8536 8.41964 19.7981C8.16335 19.41 8.20607 18.8825 8.54778 18.5408Z" fill="white" />
+                    </svg>
+                  </button>
+                </div>
+              )}
             </div>
             
             {!isMobile && (
